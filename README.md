9d550420d487e6b6cad92d9d35ec0465c4245986
onon1101 <mingzhuguly@gmail.com>
2023-07-23 00:37:22 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # delete-security-code
 移除北科登入介面的驗證碼
+
+## How to use
+1. 安裝tempermonkey
+    - [edge version](https://microsoftedge.microsoft.com/addons/detail/%E7%AF%A1%E6%94%B9%E7%8C%B4/iikmkjmpaadaobahmlepeloendndfphd?hl=zh-TW)
+    - [chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=zh-TW)
+2. 下載[程式碼](https://greasyfork.org/zh-CN/scripts/471454-delete-security-code)
+
+3. 前往[北科登入頁面](https://nportal.ntut.edu.tw/index.do)
+4. 若您有看到右上角的開關為開啟的話, 就代表已經啟用
+![](./docs/success.png)
+
+## 說明
+部分細節功能還在實踐中，如果有更好的想法也可以PR上來
---
+++ src/index.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         delete-security-code
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  解除北科的驗證碼
+// @author       onon1101
+// @license      MIT
+// @match        https://nportal.ntut.edu.tw/index.do*
+// @match        https://nportal.ntut.edu.tw/login.do
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=edu.tw
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/qs/6.11.2/qs.min.js
+// @require      http://ajax.googleapis.com/ajax/libs/dojo/1.17.3/dojo/dojo.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log('start');
+})();
+
+if (location.href === 'https://nportal.ntut.edu.tw/login.do') {
+    document.getElementsByTagName("font")[0].remove;
+    const errorMsg = document.getElementsByTagName("center")[0].innerText.split('\n')[7];
+    if (errorMsg === '「驗證碼」輸入錯誤，請重新輸入。') {
+        location.reload();
+    }
+    return ;
+}
+
+window.login1 = function login1() {
+    document.getElementById('mpassword').type = 'password';
+    document.getElementById('eyes').src = 'images/pwdhide.png';
+    var thisform = document.login;
+    var encodePwd = "{ENCODE}" + dojox.encoding.crypto.Blowfish.encrypt(thisform.mpassword.value, thisform.muid.value.toLowerCase());
+    thisform.mpassword.value = encodePwd;
+    thisform.submit();
+}
+
+document.getElementById("authcode").remove();
+
